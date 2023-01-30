a586361fe71cbcf1aa162b4c7c2f2f11e9a6ae50
Ayush Sethi <ayushsethi@google.com>
2023-01-30 10:49:55 +0000

    Adding more stats and printing them

--- perfmetrics/scripts/load_tests/python/load_generator/task.py
+++ perfmetrics/scripts/load_tests/python/load_generator/task.py
@@ -54,9 +54,12 @@ class LoadTestTask:
     time_pts = [time_pt -start_time for time_pt in kwargs['time_pts']]
     print(time_pts[-1], time_pts[0])
     actual_run_time = time_pts[-1] - time_pts[0]
+
     # cpu stats
     cpu_pts = [np.mean(all_cpus_per) for all_cpus_per in kwargs['cpu_per_pts']]
     cpu_lat_pers = self._compute_percentiles(cpu_pts)
+    avg_cpu_usage = np.mean(cpu_lat_pers)
+
     # upload and download bw stats
     upload_io_pts = []
     download_io_pts = []
@@ -66,19 +69,29 @@ class LoadTestTask:
       for _, net_io in kwargs['net_io_pts'][idx].items():
         upload_io = upload_io + net_io.bytes_sent
         download_io = download_io + net_io.bytes_recv
-      upload_io_pts.append(upload_io / (1024 * 1024 * 1024))
-      download_io_pts.append(download_io / (1024 * 1024 * 1024))
+      upload_io_pts.append(upload_io / (1024 * 1024))
+      download_io_pts.append(download_io / (1024 * 1024))
     upload_bw_pts = [0] + [(upload_io_pts[idx + 1] - upload_io_pts[idx]) / (time_pts[idx + 1] - time_pts[idx]) for idx in range(len(time_pts) - 1)]
     download_bw_pts = [0] + [(download_io_pts[idx + 1] - download_io_pts[idx]) / (time_pts[idx + 1] - time_pts[idx]) for idx in range(len(time_pts) - 1)]
     avg_upload_bw = (upload_io_pts[-1] - upload_io_pts[0]) / actual_run_time
     avg_download_bw = (download_io_pts[-1] - download_io_pts[0]) / actual_run_time
+    
     # task latency stats
     task_lat_pts = [result[3] - result[2] for result in kwargs['tasks_results_queue']]
     task_lat_pers = self._compute_percentiles(task_lat_pts)
     return {'actual_run_time': actual_run_time, 'cpu_pts': cpu_pts, 'time_pts': time_pts,
             'cpu_lat_pers': cpu_lat_pers, 'upload_bw_pts': upload_bw_pts,
             'download_bw_pts': download_bw_pts, 'avg_upload_bw': avg_upload_bw,
-            'avg_download_bw': avg_download_bw, 'task_lat_pers': task_lat_pers}
+            'avg_download_bw': avg_download_bw, 'task_lat_pers': task_lat_pers,
+            'avg_cpu_usage': avg_cpu_usage}
+
+  def _dump_results_into_json(self, results, output_dir):
+    output_dir = os.path.join('output_dir', self.TASK_NAME, "")
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+
+    with open(os.path.join(output_dir, 'metrics.json'), 'w') as fp:
+      json.dump(results, fp)
 
   def post_load_test(self, **kwargs):
     metrics = self._compute_default_post_test_metrics(**kwargs)
@@ -90,13 +103,8 @@ class LoadTestTask:
                                              [metrics['cpu_pts']],
                                              'CPU Usage (%)', 'time (sec)',
                                              '%', ['CPU'])
-
-    output_dir = os.path.join(kwargs['output_dir'] , self.TASK_NAME, "")
-    if not os.path.exists(output_dir):
-      os.makedirs(output_dir)
-    with open(os.path.join(output_dir, 'metrics.json'), 'w') as fp:
-      json.dump(metrics, fp)
-
+    output_dir = kwargs['output_dir']
+    self._dump_results_into_json(metrics, output_dir)
     bw_fig.savefig(os.path.join(output_dir, 'bandwidth.png'))
     cpu_fig.savefig(os.path.join(output_dir, 'cpu.png'))
 
--- perfmetrics/scripts/load_tests/python/tf_tasks.py
+++ perfmetrics/scripts/load_tests/python/tf_tasks.py
@@ -14,3 +14,22 @@ class ReadFromOSTask(task.LoadTestTask):
     os.close(my_file)
     return len(content)
 
+  def post_load_test(self, **kwargs):
+    default_results = self.post_load_test(**kwargs)
+    metrics = default_results['metrics']
+
+    # compute bandwidth from task results
+    total_io_bytes = sum([task_result[4]
+                          for task_result in kwargs['tasks_results_queue']])
+    avg_computed_net_bw = total_io_bytes / metrics['actual_run_time']
+    metrics.update({'avg_computed_net_bw': avg_computed_net_bw})
+
+    # dump the results
+    self._dump_results_into_json(metrics, kwargs['output_dir'])
+    print("Avg. computed BW: ", metrics['avg_computed_net_bw'])
+    print("Avg. download network BW: ", metrics['avg_download_bw'])
+    print("Avg. upload network BW: ", metrics['avg_upload_bw'])
+    print("Avg. CPU utilization(%): ", metrics[''])
+    return {'metrics': metrics}
+
+
