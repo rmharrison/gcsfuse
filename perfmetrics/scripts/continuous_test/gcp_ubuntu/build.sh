842cfc218727dff76764d0b3fd0cda942ff5da13
Tulsi Shah <tulsishah@google.com>
2023-02-16 10:12:56 +0000

    changing script to run kokoro test

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
@@ -14,6 +14,7 @@ sudo apt-get install fio -y
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
 # Get the latest commitId of yesterday in the log file
 # commitId=$(git log --before='yesterday 23:59:59' --max-count=1 --pretty=%H)
+echo "CHECKOUT .... "
 git checkout de495edcaf1839d3a15ea0d00f1cf804f2690cae
 echo Mounting gcs bucket
 mkdir -p gcs
